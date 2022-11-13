417ca705663aba8959eebaf4d6a1467af71d6b54
jinhyeokfang <jinhyeokfang@gmail.com>
2022-11-13 14:21:43 +0000

    Add Profile Wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Nanum_Gothic } from '@next/font/google'
+
+const font = Nanum_Gothic({
+  weight: "400"
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div className={font.className}><Component {...pageProps} /></div>
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,24 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Jin-hyeok Bang</title>
         <meta name="description" content="Jin-hyeok Bang" />
-        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        My Profile Here
+        <div className={styles.wrapper}>
+          <Image src="/profile.jpg" alt="profile" width="200" height="200" className={styles.profileImage}/>
+          <h1 className={styles.profileName}>방진혁</h1>
+          <h2 className={styles.profileEnglishName}>Jin-hyeok Bang</h2>
+          <span>github.com/jinhyeokfang</span>
+        </div>
       </main>
     </div>
   )
