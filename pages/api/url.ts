06a16591f5be7df5d066e148d6795c5cf9ea71ca
jinhyeokfang <jinhyeokfang@gmail.com>
2022-11-22 14:48:49 +0000

    fix: add validate logic to url api

--- pages/api/url.ts
+++ pages/api/url.ts
@@ -1,8 +1,25 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { createSite, getSite } from '../../database/actions';
 
-export async function GET(req: VercelRequest, res: VercelResponse) {
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+    if (req.method == 'GET')
+        GET(req, res);
+    else if (req.method == 'POST')
+        POST(req, res);
+    else
+        res.status(404).send({ success: false });
+}
+
+async function GET(req: VercelRequest, res: VercelResponse) {
     const name = req.query.name as string;
+
+    if (name === undefined) {
+        res.status(400).json({
+            success: false,
+            error: `this api requires query variable named 'name'.`,
+        })
+    }
+
     try {
         const siteQueryResult = await getSite(name);
         const site = siteQueryResult.Item ? siteQueryResult.Item : null;
@@ -11,14 +28,14 @@ export async function GET(req: VercelRequest, res: VercelResponse) {
             site,
         });
     } catch (error) {
-        res.json({
+        res.status(500).json({
             success: false,
             error,
         });
     }
 }
 
-export async function POST(req: VercelRequest, res: VercelResponse) {
+async function POST(req: VercelRequest, res: VercelResponse) {
     const { siteName, url } = req.body;
     try {
         await createSite(siteName, url);
@@ -26,7 +43,7 @@ export async function POST(req: VercelRequest, res: VercelResponse) {
             success: true,
         });
     } catch (error) {
-        res.json({
+        res.status(500).json({
             success: false,
             error,
         });
